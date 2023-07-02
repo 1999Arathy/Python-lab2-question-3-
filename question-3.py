293160a3f07c69e0066e34c7186785a48cd1bcb3
1999Arathy <85410988+1999Arathy@users.noreply.github.com>
2023-07-02 20:43:46 +0530

    Add files via upload

---
+++ question-3.py
@@ -0,0 +1,6 @@
+#Write a Python program to calculate the length of a string.
+a = "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+nofletters=0
+for i in a:
+    nofletters=nofletters+1
+print(nofletters)
